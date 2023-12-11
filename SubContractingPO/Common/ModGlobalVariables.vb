40fc6385b2fdbe6eef98bbe97d4a1cd0ae86495b
Harindran <harindran.m@altrockstech.com>
2023-12-11 12:00:04 +0530

    Add project files.

---
+++ SubContractingPO/Common/ModGlobalVariables.vb
@@ -0,0 +1,35 @@
+﻿Namespace SubContractingPO
+    Module ModGlobalVariables
+        Public NewLink As String = "-1"
+        Public Link_Value As String = "-1"
+        Public Link_objtype As String = "-1"
+        Public POItem As Char = ""
+        Public ItemBOM As Char = ""
+        Public GetDatePO As Char = ""
+        Public ResourceEnable As Char = ""
+        Public Costing As Char = ""
+        Public AutoProduction As Char = ""
+        Public ReceiptAutoLoad As Char = ""
+        Public APAutoLoad As Char = ""
+        Public SubPOScreen As Char = ""
+        Public AutoAssignItem As Char = ""
+        Public TypeInScrap As Char = ""
+        Public VendorSPlPrice As Char = ""
+        Public ToLocationInOutput As Char = ""
+        Public ToWhseInInput As Char = ""
+        Public ToWhseInvTransfer As Char = ""
+        Public TranList As Char = ""
+        Public BranchEnabled As Char = ""
+        Public SuperUser As String = ""
+        Public FrmMultiSel As SAPbouiCOM.Form
+        Public ProductQuery As String = ""
+        Public FieldName As String = ""
+        Public WPrice As Char = ""
+        Public PlanQtyUDFEditable As Char = ""
+        Public UDF0 As Char = ""
+        Public UDF1 As Char = ""
+        Public ValUDF0 As Char = ""
+        Public ValUDF1 As Char = ""
+        Public bModal As Boolean = False
+    End Module
+End Namespace
